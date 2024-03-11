629fa82bb65cbc548234c285d13cb3681fb807e8
LilianBarbe <110779804+LilianBarbe@users.noreply.github.com>
2024-03-11 21:12:58 +0100

    Enlever Clear All à la fin de real

--- src/realisations.js
+++ src/realisations.js
@@ -15,8 +15,6 @@ navSetup();
 applyButtonsBehav();
 
 // functions
-formWrap.insertAdjacentElement("beforeend", clearBtn);
-
 window.fsAttributes = window.fsAttributes || [];
 window.fsAttributes.push([
   "cmsfilter",
